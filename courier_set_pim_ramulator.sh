72661ecf9610cc3d7e23922ca544aee973da12bb
wwj281 <982567656@qq.com>
2025-10-23 15:09:29 +0800

    debug

--- courier_set_pim_ramulator.sh
+++ courier_set_pim_ramulator.sh
@@ -10,12 +10,12 @@ cp courier_pim_ramulator_src/pim_scheduler.cpp ramulator2/src/dram_controller/im
 cp courier_pim_ramulator_src/pim_loadstore_trace.cpp ramulator2/src/frontend/impl/memory_trace/
 cp courier_pim_ramulator_src/PIM_DRAM_system.cpp ramulator2/src/memory_system/impl/
 cp -r courier_pim_ramulator_src/trace_gen ramulator2/
-cp -r courier_pim_ramulator_src/patches1 ramulator2/
+cp -r courier_pim_ramulator_src/patches ramulator2/
 
 # Apply patches
 cd ramulator2;
 
-for f in ./patches1/*.patch
+for f in ./patches/*.patch
 do
     patch -p1 < $f
 done
