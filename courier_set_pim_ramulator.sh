bc9ab141db8488d0afd87a6349cbc3847d6a4c7a
wwj281 <982567656@qq.com>
2026-03-13 09:15:05 +0000

    debug

--- courier_set_pim_ramulator.sh
+++ courier_set_pim_ramulator.sh
@@ -9,6 +9,7 @@ cd ../../
 
 # copy files
 cp courier_pim_ramulator_src/DDR4_PIM.cpp ramulator2/src/dram/impl/
+cp courier_pim_ramulator_src/LPDDR5_PIM.cpp ramulator2/src/dram/impl/
 cp courier_pim_ramulator_src/ddr_pim_controller.cpp ramulator2/src/dram_controller/impl/
 cp courier_pim_ramulator_src/pim_scheduler.cpp ramulator2/src/dram_controller/impl/scheduler/
 cp courier_pim_ramulator_src/pim_loadstore_trace.cpp ramulator2/src/frontend/impl/memory_trace/
